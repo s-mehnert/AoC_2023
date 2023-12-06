22a3ed610c61d844729dd666c325961fe443d14c
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-12-06 22:47:24 +0100

    refactor: find all digits in one go - everything works now

--- 01_trebuchet.py
+++ 01_trebuchet.py
@@ -39,17 +39,23 @@ print("The sum of all calibration values in the document is:", sum)
 
 # Day 1 --- Part 2
 
-written_digits = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+written_digits = {
+    "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, 
+    "six": 6, "seven": 7, "eight": 8, "nine": 9, "1": 1, "2": 2, 
+    "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9
+}
 
 # find written digits:
 def find_digits(line):
     digits_found = dict()
     for digit in written_digits:
         if digit in line:
-            digits_found[line.find(digit)] = digit
+            start = 0
+            while digit in line[start:]:
+                digit_index = line.find(digit, start)
+                digits_found[digit_index] = digit
+                start = digit_index + 1               
     return digits_found
-
-digit_dict = find_digits(line)
 
 # find first written digit
 def find_first_digit(digit_dict):
@@ -57,42 +63,18 @@ def find_first_digit(digit_dict):
 
 # find last written digit
 def find_last_digit(digit_dict):
-    return digit_dict[max(digit_dict.keys())]
+    return digit_dict[max(digit_dict.keys())]  
 
-# adjust find_nums function to return indices as well
-def find_nums_and_indices(line):
-    num_list = list()
-    for i, char in enumerate(line):
-        if is_number(char):
-            num_list.append((char, i))
-    if not num_list:
-        first_num = find_digits(line)
-        last_num = find_digits(line)
-    else:
-        first_num, first_index = num_list[0]
-        last_num, last_index = num_list[-1]
-        if find_digits(line[:first_index]):
-            first_num = find_digits(line[:first_index])
-        if find_digits(line[last_index:]):
-            last_num = find_digits(line[last_index:])
-    return (first_num, last_num)    
-
-# adjust create_calibration_value function to take written digits into account
-def create_calibration_value_with_written_digits(num_list):
-    first_num, last_num = num_list
-    if isinstance(first_num, dict):
-        first_num = written_digits[find_first_digit(first_num)]
-    if isinstance(last_num, dict):
-        last_num = written_digits[find_last_digit(last_num)]
-    return int(str(first_num) + str(last_num))
+# create calibration value out of first and last digit taking written digits into account
+def create_calibration_value(digit_dict):
+    return int(str(written_digits[find_first_digit(digit_dict)]) + str(written_digits[find_last_digit(digit_dict)]))
 
 # sum calibration values
 total = 0
 
 for line in imported_data:
-    sum = create_calibration_value_with_written_digits(find_nums_and_indices(line))
-    print(sum)
+    sum = create_calibration_value(find_digits(line))
     total += sum
 
 # print result
-print("The sum of all calibration values in the document is:", total)
+print("The sum of all calibration values in the document including written digits is:", total)
