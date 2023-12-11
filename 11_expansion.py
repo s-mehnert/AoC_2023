d9743cda25907c6f98ac90feb7be11d15c2df25a
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-12-11 16:50:23 +0100

    Part 1: solved

--- 11_expansion.py
+++ 11_expansion.py
@@ -33,12 +33,6 @@ for i in range(len(expanded_universe)):
             galaxies[str(galaxy_counter)] = (i, j)
             galaxy_counter += 1
 
-for row in expanded_universe:
-    print(row)
-
-for galaxy, pos in galaxies.items():
-    print(galaxy, "--> position:", pos)
-
 # find all galaxy pairs
 
 combinations = list(galaxies)
@@ -46,18 +40,19 @@ galaxy_pairs = list()
 for i in range(len(combinations)):
     for j in range(i+1, len(combinations)):
         galaxy_pairs.append((combinations[i], combinations[j]))
-print(combinations)
-for pair in galaxy_pairs:
-    print(pair)
-print(len(galaxy_pairs))
 
 # find shortest path between two galaxies
 
-print(galaxies["9"][0])
 def find_shortest_path(gal1, gal2): # doesn't work --> find out why
-    distance1 = galaxies[gal1][0] - galaxies[gal2[0]]
-    print(distance1)
-    distance2 = galaxies[gal2][1] - galaxies[gal2][2]
-    return abs(distance1 + distance2)
+    distance1 = abs(galaxies[gal1][0] - galaxies[gal2][0])
+    distance2 = abs(galaxies[gal1][1] - galaxies[gal2][1])
+    return distance1 + distance2
 
-print(find_shortest_path("5", "9"))
+# calculate sum of shortest paths between all galaxy pairs
+
+total = 0
+
+for pair in galaxy_pairs:
+    total += find_shortest_path(*pair)
+
+print(f"The sum of the shortest paths between all galaxy pairs is {total} steps.")
