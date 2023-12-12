01bd1539114c42b3dfaf109b329c84f102e4c83f
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-12-12 14:51:29 +0100

    Day 3: import data

---
+++ 03_engine.py
@@ -0,0 +1,12 @@
+# AoC 2023 --- Day 5 
+
+# import puzzle input
+imported_data = list()
+
+with open("03_input.txt") as input:
+    for line in input.readlines():
+        imported_data.append(line.strip("\n"))
+
+print(imported_data)
+
+# Part 1
